a15642f852bfe586c1d660b61def6ae9d77cb624
Niklas Mårdby <niklas.mardby@gmail.com>
2025-03-14 11:30:55 +0100

    Add FormList component

--- src/components/FormList/FormList.tsx
+++ src/components/FormList/FormList.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import { useListContext } from "../../contexts/ListContext";
+
 export default function FormList() {
+	const { dispatch } = useListContext();
+
+	const [title, setTitle] = useState("");
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTitle(e.target.value);
+	};
+
+	const handleClick = () => {
+		dispatch({ type: "ADD", payload: title });
+	};
+
 	return (
 		<div>
 			<h1>FormList</h1>
+			<label htmlFor="title">Title: </label>
+			<input
+				type="text"
+				name="title"
+				placeholder="Title..."
+				value={title}
+				onChange={handleChange}
+			/>
+			<button onClick={handleClick}>Add</button>
 		</div>
 	);
 }
